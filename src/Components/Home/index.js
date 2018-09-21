94e8d3b2c615caed94aeebe1662fa071e33d56ec
Monzoor Morshed Mahmud <mmtamal@gmail.com>
2018-09-22 02:14:47 +0600

    :rotating_light: :lipstick: Add basic layout. Fix linting issue

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,16 +1,11 @@
 import React, { PureComponent } from 'react';
-import { Button, Pagination } from 'antd';
-// import Header from './Header';
-// import Footer from './Footer';
 
 class Home extends PureComponent {
     render() {
         return (
             <div>
                 <div className="content mt-xl--m">
-                    qweqw sdsd sdeqwe
-                    <Button type="primary">Primary</Button>
-                    <Pagination defaultCurrent={1} total={50} showSizeChanger />
+                    this is home
                 </div>
             </div>
         );
--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,20 +1,81 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import {
+    Layout,
+    Menu,
+    Breadcrumb,
+    Icon,
+} from 'antd';
 
-import Header from './Header';
-import Footer from './Footer';
+const {
+    Sider, Header, Content, Footer,
+} = Layout;
+
+const { SubMenu } = Menu;
+
+// import Header from './Header';
+// import Footer from './Footer';
 
 class MainLayout extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapsed: false,
+        };
+    }
+
+    onCollapse = (collapsed) => {
+        console.log(collapsed);
+        this.setState({ collapsed });
+    }
+
     render() {
         const { children } = this.props;
+        const { collapsed } = this.state;
         return (
-            <div>
-                <Header />
-                <div className="content mt-xl--m">
-                    {children}
-                </div>
-                <Footer />
-            </div>
+            <Layout style={{ minHeight: '100vh' }}>
+                <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+                    <div className="logo" />
+                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                        <Menu.Item key="1">
+                            <Icon type="pie-chart" />
+                            <span>Option 1</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="desktop" />
+                            <span>Option 2</span>
+                        </Menu.Item>
+                        <SubMenu
+                          key="sub1"
+                          title={(
+                              <span>
+                                  <Icon type="user" />
+                                  <span>User</span>
+                              </span>)}
+                        >
+                            <Menu.Item key="3">Tom</Menu.Item>
+                            <Menu.Item key="4">Bill</Menu.Item>
+                            <Menu.Item key="5">Alex</Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </Sider>
+                <Layout style={{ minHeight: '100vh' }}>
+                    <Header style={{ background: '#fff', padding: 0 }} />
+                    <Content style={{ margin: '0 16px' }}>
+                        <Breadcrumb style={{ margin: '16px 0' }}>
+                            <Breadcrumb.Item>User</Breadcrumb.Item>
+                            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                        </Breadcrumb>
+                    </Content>
+                    <div style={{ padding: 24, background: '#fff', minHeight: '80vh' }}>
+                        asdasd
+                        {children}
+                    </div>
+                    <Footer style={{ textAlign: 'center' }}>
+                        Ant Design ©2018 Created by Ant UED
+                    </Footer>
+                </Layout>
+            </Layout>
         );
     }
 }
