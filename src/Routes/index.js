ff8f877a6882e418341c8a04a6102fc14da30101
Monzoor Morshed Mahmud <mmtamal@gmail.com>
2018-10-08 00:50:22 +0600

    :art: Adding public private route
    Add public private route for auth

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,27 +1,44 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+    BrowserRouter as Router, Route, Switch, Redirect,
+} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 
 import MainLayout from '../Components/Layout';
+// import AuthLayout from '../Components/Layout/AuthLayout';
 import NotFound from '../Components/404';
 import Home from '../Components/Home';
+// import Login from '../Components/Auth';
+
+const fakeAuth = {
+    isAuth: false,
+    authenticate(cb) {
+        this.isAuth = true;
+        setTimeout(cb, 100);
+    },
+    signout(cb) {
+        this.isAuth = false;
+        setTimeout(cb, 100);
+    },
+};
 
 const AppRoute = ({ component: Component, layout: Layout, ...rest }) => {
     const status = Object.values({ ...rest.location.state })[0];
-    if (status === 404) {
-        const NotFoundTemp = (
-            <Layout>
-                <NotFound />
-            </Layout>);
-        return (<Route {...rest} render={() => (<NotFoundTemp />)} />);
-    }
-    const Main = () => (
+
+    let LayoutToRender = () => (
         <Layout>
             <Component />
         </Layout>
     );
-    return (<Route {...rest} render={props => (<Main {...props} />)} />);
+    if (status === 404) {
+        LayoutToRender = () => (
+            <Layout>
+                <NotFound />
+            </Layout>);
+    }
+
+    return (<Route {...rest} render={props => ((rest.type === 'private') ? ((fakeAuth.isAuth === true) ? <LayoutToRender {...props} /> : <Redirect to="/login" />) : <LayoutToRender {...props} />)} />);
 };
 
 AppRoute.propTypes = {
@@ -33,8 +50,9 @@ AppRoute.propTypes = {
 const Switches = () => (
     <Router>
         <Switch>
-            <AppRoute path="/dashboard" exact component={Home} layout={MainLayout} />
-            <AppRoute path="*" exact layout={MainLayout} component={NotFound} status={404} />
+            <AppRoute path="/dashboard" type="public" exact component={Home} layout={MainLayout} />
+            <AppRoute path="/private" type="private" exact component={Home} layout={MainLayout} />
+            <AppRoute path="*" exact component={NotFound} layout={MainLayout} status={404} />
         </Switch>
     </Router>
 );
