4d6698011efc76735e04c3acc3924e51a9ae96c8
Monzoor Morshed Mahmud <mmtamal@gmail.com>
2018-11-24 18:38:33 +0600

    :white_check_mark: Add test case fog login

--- src/Components/Auth/Login.test.js
+++ src/Components/Auth/Login.test.js
@@ -1,88 +1,117 @@
-// import Login from '../Home/index';
-// import { Login } from './Login';
-// import { BrowserRouter as Router } from 'react-router-dom';
-// import ReactDOM from 'react-dom';
-// import { MemoryRouter } from 'react-router';
-import { Login } from './Login';
-import { Form } from 'antd';
-// import NotFoundPage from '../404';
-// import App from '../App';
+import React from 'react';
+import { Provider } from 'react-redux';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { MemoryRouter } from 'react-router';
+// eslint-enable-next-line import/no-extraneous-dependencies
+import LoginFormCompoent, { Login, LoginForm } from './Login';
+import { Email, Password } from './_FormComponent';
+import App from '../App';
+// import { mountWrap } from './testHelper';
+import store from '../../Store';
+
+const props = {
+    errors: {
+        errorInfos: {
+            hasErrors: false,
+        },
+    },
+    dispatch: jest.fn(),
+};
+let formRef;
+
+const wrappedMount = () => mountWrap(<Provider store={store}><LoginForm wrappedComponentRef={(inst) => { formRef = inst; }} {...props} /></Provider>);
+
+wrappedMount();
+const { form } = formRef.props;
+
 
 describe('This is login test', () => {
-    // it('add correctly', () => {
-    //     expect(1 + 1).toEqual(2);
-    // });
-    const fakeEvent = { preventDefault: () => console.log('preventDefault') };
-    // const loginComponent = shallow(<LoginForm />);
-    it('found login from', () => {
-        // const wrapper = mount(
-        //     <MemoryRouter initialEntries={['/random']}>
-        //         <App />
-        //     </MemoryRouter>,
-        // );
-        // expect(wrapper.find(LoginForm)).toHaveLength(0);
-        // expect(wrapper.find(NotFoundPage)).toHaveLength(1);
-        // const wrapper = mount(<Router><LoginForm /></Router>);
-        // const push = jest.fn();
-        // wrapper.setProps({ history: { push } });
-        // const LoginTest = Form.create()(Login);
-        // Form.setFieldsValue({
-        //     email: {
-        //         value: 'asdasd@f.com',
-        //     },
-        // });
-        // const loginComponent = shallow(<LoginForm wrappedComponentRef={(form) => this.form = form} />);
-        let formRef= {
-            form: {},
-        };
-        const loginComponent = shallow(<Login{...formRef} />);
-        // console.log(loginComponent.instance().props.wrappedComponentRef());
-        // loginComponent.getDOMNode().setFieldsValue({
-        //     name: 'tom'
-        //   });
-        // expect(loginComponent.node.getFieldsValue()).toEqual({name: 'tom'});
-        // console.log()
-        expect(loginComponent.find('#logIn').length).toBe(1);
-        // loginComponent.find('#logIn').simulate('submit', fakeEvent);
-        // const loginComponent = shallow(<LoginForm />).dive();
-        // console.log(loginComponent.debug());
-        // // expect(loginComponent.find('#logIn')).to.have.lengthOf(1);
-        // // expect(loginComponent.find('#logIn').length).toBe(1);
-        // expect(loginComponent.findWhere(node => node.hasClass('test'))).toBe(1);
+    // Done test
+    it('Find login component', () => {
+        const appWrapper = mount(
+            <MemoryRouter>
+                <App />
+            </MemoryRouter>,
+        );
+        expect(appWrapper.find(LoginFormCompoent).length).toBe(1);
     });
-    it('User form submit test', () => {
-        let formRef= {
-            form: {
-                setFields: {
-                    email: {
-                        value: 'asdasd',
-                    },
+    it('Email validation on change with no input', () => {
+        const wrapper = shallow(<Login form={form} {...props} />);
+        const emailComponent = shallow(<Email form={form} />);
+
+        emailComponent.find('#email').simulate('change');
+        expect(wrapper.find('Email').render().find('.ant-form-explain').text()).toEqual('email is required');
+    });
+    it('Email validation on change with wrong email format', () => {
+        const wrapper = shallow(<Login form={form} {...props} />);
+        const emailComponent = shallow(<Email form={form} />);
+
+        emailComponent.find('#email').simulate('change', { target: { value: 'mmtamal@gmail.c' } });
+        expect(wrapper.find('Email').render().find('.ant-form-explain').text()).toEqual('email is not a valid email');
+    });
+    it('Email validation on change with valid email format', () => {
+        const wrapper = shallow(<Login form={form} {...props} />);
+        const emailComponent = shallow(<Email form={form} />);
+
+        emailComponent.find('#email').simulate('change', { target: { value: 'mmtamal@gmail.com' } });
+        expect(wrapper.find('Email').render().find('.ant-form-explain').length).toBe(0);
+    });
+    it('Password validation length on change with no input', () => {
+        const wrapper = shallow(<Login form={form} {...props} />);
+        const passwordComponent = shallow(<Password form={form} />);
+
+        passwordComponent.find('#password').simulate('change');
+        expect(wrapper.find('Password').render().find('.ant-form-explain').text()).toEqual('password is required');
+    });
+    it('Password validation length on change with wrong length', () => {
+        const wrapper = shallow(<Login form={form} {...props} />);
+        const passwordComponent = shallow(<Password form={form} />);
+
+        passwordComponent.find('#password').simulate('change', { target: { value: '12' } });
+        expect(wrapper.find('Password').render().find('.ant-form-explain').text()).toEqual('password must be at least 3 characters');
+    });
+    it('Password validation length on change with correct length', () => {
+        const wrapper = shallow(<Login form={form} {...props} />);
+        const passwordComponent = shallow(<Password form={form} />);
+
+        passwordComponent.find('#password').simulate('change', { target: { value: '123' } });
+        expect(wrapper.find('Password').render().find('.ant-form-explain').length).toBe(0);
+    });
+
+    it('Form submit test', () => {
+        const preventDefault = jest.fn();
+        const wrapper = shallow(<Login form={form} {...props} />);
+        const emailComponent = shallow(<Email form={form} />);
+        const passwordComponent = shallow(<Password form={form} />);
+        emailComponent.find('#email').simulate('change', { target: { value: 'mmtamal@gmail.com' } });
+        passwordComponent.find('#password').simulate('change', { target: { value: '123' } });
+        wrapper.find('Form').simulate('submit', { preventDefault });
+        wrapper.setState({ loading: true });
+        expect(preventDefault).toBeCalled();
+        expect(wrapper.state().loading).toBe(true);
+    });
+
+    it('Test cGDSFP', () => {
+        const nextProps = {
+            errors: {
+                errorInfos: {
+                    hasErrors: true,
+                    messages: 'error found',
+                    componentError: true,
                 },
-                validateFields: () => {},
             },
         };
-        const loginComponent = shallow(<Login {...formRef} />);
-        loginComponent.find('#logIn').simulate('submit', fakeEvent);
-    });
-    it('Noti test', () => {
-        let formRef= {
-            form: {
-                setFields: {
-                    email: {
-                        value: 'asdasd',
-                    },
+        const prevState = {
+            errors: {
+                errorInfos: {
+                    hasErrors: false,
                 },
-                validateFields: () => {},
-                getFieldProps: (props) => {},
             },
         };
-        const loginComponent = shallow(<Login {...formRef} />);
-        // done// expect(loginComponent.find(Notification).length).toBe(1);
-        // done// expect(loginComponent.exists()).toBe(true);
-        // loginComponent.find('Email').simulate('change', {
-        //     target: { value: 'oka'}
-        // })
-        // console.log(loginComponent.find('Email'));
-        // expect(loginComponent.find('Email').props().form.getFieldProps('email').value).toBe('oka');
-    });        
+        const result = Login.getDerivedStateFromProps(nextProps, prevState);
+        expect(result).toEqual({
+            loading: false,
+            errors: nextProps.errors,
+        });
+    });
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -10,6 +10,33 @@ import {
 } from 'enzyme-to-json';
 import sinon from 'sinon';
 
+import { BrowserRouter } from 'react-router-dom';
+
+import { shape } from 'prop-types';
+
+// Instantiate router context
+const router = {
+    history: new BrowserRouter().history,
+    route: {
+        location: {},
+        match: {},
+    },
+};
+
+const createContext = () => ({
+    context: { router },
+    childContextTypes: { router: shape({}) },
+});
+
+export function mountWrap(node) {
+    return mount(node, createContext());
+}
+
+export function shallowWrap(node) {
+    return shallow(node, createContext());
+}
+
+
 // Set the default serializer for Jest to be the from enzyme-to-json
 // This produces an easier to read (for humans) serialized format.
 expect.addSnapshotSerializer(createSerializer({
@@ -22,8 +49,9 @@ Enzyme.configure({
 });
 
 // Define globals to cut down on imports in test files
-global.React = React;
+// global.React = React;
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
 global.sinon = sinon;
+global.mountWrap = mountWrap;
