a1e261f80b6ae52980e2a6fc90ff93507c88068a
Monzoor Morshed Mahmud <mmtamal@gmail.com>
2018-12-05 02:42:19 +0600

    :lipstick: Product route change, ui change

---
+++ src/Components/Product/Add.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ProductAdd extends Component {
+    render() {
+        return (
+            <div className="row">
+                <div className="col-12">
+                    <h2 className="font-weight-light">Product List</h2>
+                    <p className="text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, delectus!</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ProductAdd;
