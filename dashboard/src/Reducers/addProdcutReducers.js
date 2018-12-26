28df6fdd1154fc74f7051f7ee222c0bd5ca4d31a
Monzoor Morshed Mahmud <mmtamal@gmail.com>
2018-12-26 23:44:23 +0600

    :white_check_mark: Testing code

--- dashboard/src/Reducers/addProdcutReducers.js
+++ dashboard/src/Reducers/addProdcutReducers.js
@@ -22,6 +22,7 @@ export default function (state = initialState, action) {
             currentSteps: {
                 item: 1,
                 status: 'finish',
+                oka: 'oka',
             },
             images: action.images,
         };
